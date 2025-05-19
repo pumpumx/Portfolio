e942070b26d83480fbffe456f086828ff2159f50
pumpumx <trishuchibhot14@gmail.com>
2025-05-20 01:55:50 +0530

    Made some changes to the footer section

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -40,28 +40,28 @@ function Footer() {
                 <div className="OtherText mt-10">
                     <p className="font-bold text-xs text-white text-center lg:text-3xl">I'm available for full-time roles & freelance projects.</p>
                     <span>
-                        <p className="text-xs text-white text-center lg:text-2xl">I thrive on crafting dynamic web applications, and <br />
+                        <p className="text-xs text-white/70 text-center lg:text-2xl">I thrive on crafting dynamic web applications, and <br />
                             delivering seamless user experiences.</p>
                     </span>
                 </div>
             </div>
 
-            <div className="w-full h-[30vh] flex">
+            <div className="w-full h-[30vh] flex ">
                 <div className="aboutDiv w-[50%] flex flex-col justify-center items-center">
                     <div className="logo  w-[4rem] ">
                         <img src="/src/assets/logo.png" className="w-[4rem]" alt="Hey this is logo" />
                     </div>
-                    <div className="text">
-                        <p className="text-xs font-normal text-white text-center">I'm Naaz Manhas - a full stack web developer,
+                    <div className="text w-[50%]">
+                        <p className="text-xs font-normal text-white text-center lg:text-lg ">I'm Naaz Manhas - a full stack web developer,
                             freelancer & problem solver. Thanks for checking out my site!
                         </p>
                         <br />
                         <p className="text-xs font-normal text-white text-center">	&#169; Naaz Manhas </p>
                     </div>
                 </div>
-                <div className="otherDiv w-[50%] flex items-center justify-around ">
+                <div className="otherDiv w-[40%] flex items-center justify-around ">
                     <div className="w-[25%] h-full  flex flex-col justify-center gap-2 cursor-pointer  ml-2 items-center">
-                        <div><p className="text-xs text-white font-bold text-center">General</p></div>
+                        <div><p className="text-xs lg:text-xl text-white font-bold text-center">General</p></div>
                         {FooterData.map(({ id, name, gradient }) => (
                             <div className={`${gradient} text-white text-xs hover:underline`} key={id}>
                                 <p className="">{name}</p>
@@ -69,7 +69,7 @@ function Footer() {
                         ))}
                     </div>
                     <div className="w-[25%] h-full flex flex-col justify-center gap-2 items-center cursor-pointer hover:underline">
-                        <div><p className="text-xs text-white font-bold text-center">Extra</p></div>
+                        <div><p className="text-xs text-white lg:text-xl font-bold text-center">Extra</p></div>
                         {FooterExtraData.map(({ id, name, gradient }) => (
                             <div className={`${gradient} text-white text-xs  hover:underline`} key={id}>
                                 <p className="text-white">{name}</p>
